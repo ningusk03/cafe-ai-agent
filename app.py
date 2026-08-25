f8a88c43b901ebfa3277d720bcff94e1f829b9f8
ningusk03 <ningusk59@gmail.com>
2026-08-25 13:27:09 +0530

    Change model from 'gemma2-9b-it' to 'llama-3.3-70b-versatile'

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if prompt := st.chat_input("Ask something (e.g., 'What is on the menu?')"):
                     messages.append({"role": m["role"], "content": m["content"]})
 
                 response = client.chat.completions.create(
-                   model="gemma2-9b-it",
+                   model="llama-3.3-70b-versatile",
                     messages=messages,
                 )
                 bot_reply = response.choices[0].message.content
